c4061e18b02dc5585ab6ba14edfee377cbbd8eac
Wendy Phillips <wendyphillips@Wendys-Mac-mini.local>
2023-09-07 15:40:14 -0500

    subsetting pounds and grams

--- code/tri_exploratory_code.R
+++ code/tri_exploratory_code.R
@@ -9,7 +9,9 @@ col_types
 
 # Change some columns incorrectly of type numeric
 char_cols <- c(1, 3, 9,19, 28:33,38)
-tri[,char_cols] <- as.character(tri[,char_cols])
+for (i in char_cols){
+  tri[,i] <- as.character(tri[,i])
+}
 
 # Check data types again
 str(tri)
@@ -29,6 +31,17 @@ tri_sub %>% select(where(is.numeric)) %>%
 tri_sub2 <- tri_sub |> janitor::clean_names()
 colnames(tri_sub2)
 colnames(tri_sub2) <- sub(".*?_", "", colnames(tri_sub2))
+str(tri_sub2)
+
+# Industry sectors ----
+industry_sectors <- tri_sub2 |> group_by(industry_sector) |> count(sort = TRUE)
+industry_sectors$industry_sector <- factor(industry_sectors$industry_sector, levels = industry_sectors$industry_sector)
+
+ggplot(industry_sectors, aes(y = industry_sector, x = n)) +
+  geom_bar(stat = 'identity', fill = 'skyblue', color = 'white') +
+  theme_classic() +
+  scale_x_continuous(expand = c(0,0)) +
+  labs(x = "Number of entries", y = "Industry sector", title = "All")
 
 # Chemical types ----
 # Select some chemicals of interest
@@ -78,12 +91,37 @@ ggplot(proportions_df, aes(y = feature, x = n, fill = present)) +
   scale_x_continuous(expand = c(0,0)) +
   scale_fill_brewer(type = 'qual', palette = 3)
 
+# Grams v pounds -----
+# Remember releases have different measures!
+table(tri_sub2$unit_of_measure)
+
+# Subset to entries in pounds
+in_pounds <- tri_sub2[tri_sub2$unit_of_measure == "Pounds",]
+
+# Dioxins ----
+# Subset to just entries in grams
+in_grams <- tri_sub2[tri_sub2$unit_of_measure == "Grams",]
+
+# Are all in_grams entries for dioxin?
+table(in_grams$classification)
+
+# Dioxin by state
+per_state_dioxin <- in_grams |> group_by(st) |> count(sort = T)
+
+table(in_grams$industry_sector)
+
 # By state -----
 # Investigate entries per state
 per_state <- tri_sub2 |> group_by(st) |> count(sort = T)
 
+chem_combo_releases <- in_pounds |> 
+  group_by(metal, pbt,elemental_metal_included, clean_air_act_chemical, pfas, carcinogen ) |> 
+  summarize(tot_rel = sum(total_releases),
+            on_site = sum(on_site_release_total),
+            off_site = sum(off_site_release_total))
+
 # Which states have highest total releases?
-per_state_totals <- tri_sub2 |> 
+per_state_totals <- in_pounds |> 
   group_by(st) |> 
   summarize(tot_releases = sum(total_releases)) |>
   dplyr::arrange(desc(tot_releases))
@@ -95,10 +133,10 @@ ggplot(per_state_totals, aes(y = st, x = tot_releases)) +
   geom_bar(stat = 'identity', fill = 'skyblue', color = 'white') +
   theme_classic() +
   scale_x_log10(expand = c(0,0)) +
-  labs(x = "Total releases", y = "State/Territory", title = "All")
+  labs(x = "Total releases (pounds)", y = "State/Territory", title = "All")
 
 # Which states have highest metal releases?
-per_state_metal <- tri_sub2 |> 
+per_state_metal <- in_pounds |> 
   dplyr::filter(metal == 'YES') |> 
   group_by(st) |> 
   summarize(tot_releases = sum(total_releases)) |>
@@ -111,4 +149,4 @@ ggplot(per_state_metal, aes(y = st, x = tot_releases)) +
   geom_bar(stat = 'identity', fill = 'skyblue', color = 'white') +
   theme_classic() +
   scale_x_log10(expand = c(0,0)) +
-  labs(x = "Total releases", y = "State/Territory", title = "Metal")
+  labs(x = "Total releases (pounds)", y = "State/Territory", title = "Metal")
