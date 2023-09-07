f0e7198f047f3650f92f0abc24a4001f2d0ff971
Wendy Phillips <wendyphillips@Wendys-Mac-mini.local>
2023-09-07 13:46:12 -0500

    adding more investigatory code

--- code/tri_exploratory_code.R
+++ code/tri_exploratory_code.R
@@ -4,10 +4,10 @@ library(janitor)
 tri <- read.csv('data/tri_2022_us.csv', sep = ",", check.names = FALSE)
 
 # Check data types
-str(tri)
-str(tri[,99:119])
+col_types <-data.frame(sapply(tri, class))
+col_types
 
-# Change some columns incorrectly in numeric
+# Change some columns incorrectly of type numeric
 char_cols <- c(1, 3, 9,19, 28:33,38)
 tri[,char_cols] <- as.character(tri[,char_cols])
 
@@ -30,17 +30,37 @@ tri_sub2 <- tri_sub |> janitor::clean_names()
 colnames(tri_sub2)
 colnames(tri_sub2) <- sub(".*?_", "", colnames(tri_sub2))
 
+# Chemical types ----
+# Select some chemicals of interest
+chem_of_interest <- colnames(tri_sub2[c(13,14,16,18:20)])
+chem_of_interest
+
+# Investigate which of those chemical types co-occur
+chem_combo_counts <- tri_sub2 |> group_by(metal, pbt,elemental_metal_included, clean_air_act_chemical, pfas, carcinogen ) |> count(sort = TRUE)
+chem_combo_counts$proportion <- chem_combo_counts$n/sum(chem_combo_counts$n)
+
 # Create a dataframe with proportion of entries
 #  with certain chemical types present
-proportions_df <- data.frame(present = as.character(), n = as.numeric(), percent=as.numeric(), feature = as.character())
-for (nm in colnames(tri_sub2[c(13,14,16,18:20)])){
-  temp_df<-tri_sub2 %>% janitor::tabyl(nm)
+proportions_df <- data.frame(
+  present = as.character(), 
+  n = as.numeric(), 
+  percent=as.numeric(), 
+  feature = as.character()
+  )
+
+for (nm in chem_of_interest){
+  # get table of n and % samples each category
+  temp_df <- tri_sub2 %>% janitor::tabyl(nm)
+
   colnames(temp_df)[1] <- "present"
+  
+  # Add chemical type to column
   temp_df$feature <- nm
+  
   proportions_df <- rbind(proportions_df, temp_df)
 }
 
-# Factor features in order of percent present
+# Factor features in order of percent present for plotting
 proportions_df <- proportions_df |> dplyr::arrange(present, percent)
 proportions_df$feature <- factor(proportions_df$feature, levels = unique(proportions_df$feature)) 
 
@@ -57,3 +77,38 @@ ggplot(proportions_df, aes(y = feature, x = n, fill = present)) +
   theme_classic() +
   scale_x_continuous(expand = c(0,0)) +
   scale_fill_brewer(type = 'qual', palette = 3)
+
+# By state -----
+# Investigate entries per state
+per_state <- tri_sub2 |> group_by(st) |> count(sort = T)
+
+# Which states have highest total releases?
+per_state_totals <- tri_sub2 |> 
+  group_by(st) |> 
+  summarize(tot_releases = sum(total_releases)) |>
+  dplyr::arrange(desc(tot_releases))
+
+# Factor for plotting
+per_state_totals$st <- factor(per_state_totals$st, levels = per_state_totals$st)
+
+ggplot(per_state_totals, aes(y = st, x = tot_releases)) +
+  geom_bar(stat = 'identity', fill = 'skyblue', color = 'white') +
+  theme_classic() +
+  scale_x_log10(expand = c(0,0)) +
+  labs(x = "Total releases", y = "State/Territory", title = "All")
+
+# Which states have highest metal releases?
+per_state_metal <- tri_sub2 |> 
+  dplyr::filter(metal == 'YES') |> 
+  group_by(st) |> 
+  summarize(tot_releases = sum(total_releases)) |>
+  dplyr::arrange(desc(tot_releases))
+
+# Factor for plotting
+per_state_metal$st <- factor(per_state_metal$st, levels = per_state_metal$st)
+
+ggplot(per_state_metal, aes(y = st, x = tot_releases)) +
+  geom_bar(stat = 'identity', fill = 'skyblue', color = 'white') +
+  theme_classic() +
+  scale_x_log10(expand = c(0,0)) +
+  labs(x = "Total releases", y = "State/Territory", title = "Metal")
